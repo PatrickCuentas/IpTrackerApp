082eebf55bc8d24b7b08a5c2eeba1c3377765424
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-03-07 20:56:40 -0500

    add the leaflet map component and icon

---
+++ Icon.js
@@ -0,0 +1,13 @@
+import L from 'leaflet'
+
+export const iconPerson = new L.Icon({
+  iconUrl: './public/images/icon-location.svg',
+  iconRetinaUrl: './public/images/icon-location.svg',
+  iconAnchor: null,
+  popupAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(60, 75),
+  className: 'leaflet-div-icon icon',
+})
---
+++ src/components/Map.jsx
@@ -0,0 +1,37 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet'
+import { iconPerson } from '../../Icon'
+function LocationMarker({ coords }) {
+  const map = useMapEvents({
+    mouseover: () => {
+      map.flyTo(coords, map.getZoom())
+    },
+  })
+
+  return coords === null ? null : (
+    <Marker position={coords} icon={iconPerson}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+export const Map = ({ coords }) => {
+  return (
+    <MapContainer
+      className="h-screen -mt-[200px] md:-mt-[168px]"
+      center={{ lat: 51.505, lng: -0.09 }}
+      zoom={13}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker coords={coords} />
+    </MapContainer>
+  )
+}
